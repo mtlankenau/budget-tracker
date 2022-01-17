a3bd256dd18a9f472248c61214cbdcd87523cb1d
mtlankenau <mtlankenau@gmail.com>
2022-01-16 23:10:59 -0500

    continued to test idb functionality, want to test on heroku deployment next

--- public/js/idb.js
+++ public/js/idb.js
@@ -9,7 +9,7 @@ request.onupgradeneeded = function(event) {
   // save a reference to the database
   const db = event.target.result;
   // create an object store (table) called 'new_budget', set it to have an auto incrementing primary key of sorts
-  db.createObjectStore('new_budget', { autoIncrement: true });
+  db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 // upon a successful 
@@ -17,7 +17,7 @@ request.onsuccess = function(event) {
   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
-  // check if app is online, if yes run uploadPizza() function to send all local db data to api
+  // check if app is online, if yes run uploadBudget() function to send all local db data to api
   if (navigator.onLine) {
     uploadBudget();
   }
@@ -30,22 +30,26 @@ request.onerror = function(event) {
 
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
-  // open a new transaction with the database with read and write permissions 
-  const transaction = db.transaction(['new_budget'], 'readwrite');
+  console.log(record);
+  // open a new transaction with the database with read and write permission;s 
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+  console.log(transaction);
 
   // access the object store for `new_budget`
-  const budgetObjectStore = transaction.objectStore('new_budget');
+  const budgetObjectStore = transaction.objectStore('new_transaction');
+  console.log(budgetObjectStore);
 
   // add record to your store with add method
   budgetObjectStore.add(record);
+  console.log(budgetObjectStore.add(record));
 }
 
 function uploadBudget() {
   // open a transaction on your db
-  const transaction = db.transaction(['new_budget'], 'readwrite');
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
 
   // access your object store
-  const budgetObjectStore = transaction.objectStore('new_budget');
+  const budgetObjectStore = transaction.objectStore('new_transaction');
 
   // get all records from store and set to a variable
   const getAll = budgetObjectStore.getAll();
@@ -54,7 +58,7 @@ function uploadBudget() {
   getAll.onsuccess = function() {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transaction/bulk', {
+      fetch('/api/transaction', {
         method: 'POST',
         body: JSON.stringify(getAll.result),
         headers: {
@@ -68,9 +72,9 @@ function uploadBudget() {
             throw new Error(serverResponse);
           }
           // open one more transaction
-          const transaction = db.transaction(['new_budget'], 'readwrite');
+          const transaction = db.transaction(['new_transaction'], 'readwrite');
           // access the new_pizza object store
-          const budgetObjectStore = transaction.objectStore('new_budget');
+          const budgetObjectStore = transaction.objectStore('new_transaction');
           // clear all items in your store
           budgetObjectStore.clear();
 
